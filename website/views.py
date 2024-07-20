4a66f9c721d794ae13b69025b8f32030207e06f1
Fahad Aamir <fhdaamir@gmail.com>
2024-07-20 14:15:33 +0500

    Final Project

--- website/views.py
+++ website/views.py
@@ -74,8 +74,22 @@ def add_record(request):
             if form.is_valid():
                 add_record=form.save()
                 messages.success(request,"Record Added...")
-            return redirect('home')
+                return redirect('home')
         return render(request, 'add_record.html', {'form':form})
     else:
         messages.success(request,"You Must Be Logged In..")
         return redirect('home')
+    
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Record Has Been Updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request,"You Must Be Logged In..")
+        return redirect('home')
+    
